0d90bd27cfa7f5e3ac3825aafda246b16d07d775
a22a-dev <a22a-dev@users.noreply.github.com>
2021-08-22 13:54:25 +0000

    deploy: 9b00107e05c4302e153fbd4100305eb1cbb4b940

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dir: {
-      input: ".", // was . //
-      includes: "_includes",
-      data: "_data",
-      output: "docs/" // was _site, I changed for github
-    }
-};
