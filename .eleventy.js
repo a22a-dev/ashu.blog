6615f2aeb55072a2cf0533cf92fb2d61068276ce
a22a-dev <a22a-dev@users.noreply.github.com>
2021-08-22 13:22:48 +0000

    deploy: 701caec61b9df50c7837a06c92edb8d96914c590

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dir: {
+      input: ".", // was . //
+      includes: "_includes",
+      data: "_data",
+      output: "docs/" // was _site, I changed for github
+    }
+};
+return {
+ pathPrefix: "/blogspace/"
+}
